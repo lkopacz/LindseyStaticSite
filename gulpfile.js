098cb01f5a43fe7c3f8b99985aea435989fc5a7c
Rich Allen <lindsey.kopacz@gmail.com>
2016-09-20 20:13:04 -0400

    removing node modules and rebuilding. Building gulpfile from scrtach

--- gulpfile.js
+++ gulpfile.js
@@ -1,70 +1,11 @@
-// Include gulp
+//Requires gulp
 var gulp = require('gulp');
 
-// Include plugins
+// Requires the gulp-sass plugin
 var sass = require('gulp-sass');
-var sourcemaps = require('gulp-sourcemaps');
-var autoprefixer = require('gulp-autoprefixer');
-var jshint = require('gulp-jshint');
-var browserSync = require('browser-sync').create();
-var browserify = require('browserify');
-var babelify = require('babelify');
-var notify = require('gulp-notify');
-var source = require('vinyl-source-stream');
 
-// Compile Sass, run autoprefixer, and create sourcemaps
-gulp.task('styles', function () {
+gulp.task('sass', function(){
   return gulp.src('source/scss/**/*.scss')
-    .pipe(sass().on("error", notify.onError("Error: <%= error.message %>")))
-    .pipe(autoprefixer({
-      browsers: ['last 2 versions', 'ie >= 8'],
-      cascade: false
-    }))
-    .pipe(sourcemaps.init())
-    .pipe(sourcemaps.write('.'))
-    .pipe(gulp.dest('stylesheets'))
-    .pipe(notify({ message: 'sass task complete' }));
+	.pipe(sass()) // Using gulp-sass
+	.pipe(gulp.dest('stylesheets'))
 });
-
-// Lint JS
-gulp.task('lint', function() {
-  return gulp.src('source/javascript/*.js')
-    .pipe(jshint('.jshintrc'))
-    .pipe(jshint.reporter('default'))
-    .pipe(notify({ message: 'Linting complete' }));
-});
-
-// Browserify Scripts
-gulp.task('browserify', function() {
-  return browserify('source/javascript/script.js', { debug: true })
-    .transform(babelify)
-    .bundle()
-    //Pass desired output filename to vinyl-source-stream
-    .pipe(source('source/javascript.js'))
-    // Start piping stream to tasks!
-    .pipe(gulp.dest('js'));
-});
-
-// Watch for changes
-gulp.task('watch', ['styles', 'browserify'], function() {
-
-  // Watch .scss files
-  gulp.watch('source/scss/*.scss', ['styles']);
-
-  // Watch .js files
-  gulp.watch('	/*.js', ['lint']);
-  gulp.watch('source/javascript/*.js', ['browserify']);
-
-  // Create Browsersync server
-  browserSync.init({
-    server: {
-      baseDir: "./"
-    }
-  });
-
-  // Watch any build files, reload on change
-  gulp.watch(['index.html', 'stylesheets/', 'js/']).on('change', browserSync.reload);
-});
-
-// Default task
-gulp.task('default', ['watch']);
