845e8cf7eebd92fd60caf6f863c66890e15ee7b0
Rich Allen <lindsey.kopacz@gmail.com>
2016-09-20 21:12:45 -0400

    updated gulpfile and tested gulp watch for sass files...not working yet, still need to figure stuff out

--- gulpfile.js
+++ gulpfile.js
@@ -4,8 +4,16 @@ var gulp = require('gulp');
 // Requires the gulp-sass plugin
 var sass = require('gulp-sass');
 
+//Requires the gulp-watch plugin
+var watch = require('gulp-watch');
+
 gulp.task('sass', function(){
   return gulp.src('source/scss/**/*.scss')
 	.pipe(sass()) // Using gulp-sass
 	.pipe(gulp.dest('stylesheets'))
 });
+
+gulp.task('watch', function(){
+  gulp.watch('source/scss/**/*.scss', ['sass']); 
+  // Other watchers
+})
