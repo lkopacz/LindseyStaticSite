fa600a16f6f3aa1c812e7e281ba229b96180ebda
Lindsey Kopacz <lindsey.kopacz@Lindsey-MacBook-Pro.local>
2016-10-04 13:53:59 -0400

    keeping jquery more literate lol

--- source/javascript/script.js
+++ source/javascript/script.js
@@ -1,7 +1,4 @@
 $(".toggle-nav").click(function(e) {
-	//the mobile menu items to appear
-	//the keep the three lines so that I can toggle back and forth
-	//click on the three lines and change the class back to the display I had before
         $(this).toggleClass('active');
         $('.menu ul').toggleClass('active');
         $('.menu ul').toggleClass('mobile');
